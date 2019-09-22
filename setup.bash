ad5f64d2d81c03492a66398e413899cfdfcb4e95
Nikhil Suresh <niksure@nus11814-12-niksure.nueva.local>
2019-09-22 15:23:43 -0700

    made dir to put tar contents in

--- setup.bash
+++ setup.bash
@@ -4,7 +4,7 @@ brew cask install visual-studio-code
 # Needs to be updated yearly for new WPILib release
 brew install wget
 wget -O ~/wpilibtar "https://github.com/wpilibsuite/allwpilib/releases/download/v2019.4.1/WPILib_Mac-2019.4.1.tar.gz"
-tar -C ~/frc2019 -xvf ~/wpilibtar
+mkdir ~/frc2019 && tar -C ~/frc2019 -xvf ~/wpilibtar
 cd ~/frc2019
 python tools/ToolsUpdater.py 
 code --install-extension vsCodeExtensions/JavaLang.vsix --install-extension vsCodeExtensions/JavaDeps.vsix --install-extension vsCodeExtensions/JavaDebug.vsix --install-extension vsCodeExtensions/WPILib.vsix
