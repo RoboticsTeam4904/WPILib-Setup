5e40ac780d96a9fd3801e1350fa209f5d05610e1
Nikhil Suresh <niksure@nus11814-12-niksure.nueva.local>
2019-09-26 12:23:02 -0700

    install git too

--- setup.bash
+++ setup.bash
@@ -2,6 +2,7 @@
 brew cask install visual-studio-code
 
 # Needs to be updated yearly for new WPILib release
+brew install git
 brew install wget
 wget -O ~/wpilibtar "https://github.com/wpilibsuite/allwpilib/releases/download/v2019.4.1/WPILib_Mac-2019.4.1.tar.gz"
 mkdir ~/frc2019 && tar -C ~/frc2019 -xvf ~/wpilibtar
